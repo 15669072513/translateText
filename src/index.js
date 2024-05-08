753b4980d5400ed30726ecd1fa5fe36bbb83ae06
lhq01844380@antgroup.com <lhq01844380@antgroup.com>
2024-05-08 14:46:04 +0800

    1111

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ async function getStarted() {
         core.debug("todir:" + toDir);
         core.debug("to:" + to);
         core.debug("head:" + context.payload.pull_request);
+        core.debug("branchname:" +  process.env.GITHUB_HEAD_REF);
 
         // var text = "你好" +   "\n";
         // await translateContent(text);
